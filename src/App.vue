5333a4e08abd23ae58b52b368cbbcbeaee930609
owentr1369 <tamvaa1306@gmail.com>
2022-05-28 10:17:39 +0700

    styling search bar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" name="search-bar" placeholder="Search..." />
+        <input
+          type="text"
+          class="search-bar"
+          name="search-bar"
+          placeholder="Search..."
+        />
       </div>
     </main>
   </div>
@@ -27,5 +32,44 @@ export default {
 }
 body {
   font-family: "montserrat", sans-serif;
+}
+#app {
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.1s;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+  .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    border: none;
+    outline: none;
+    background: none;
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+    &:focus {
+      box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+      background-color: rgba(255, 255, 255, 0.75);
+      border-radius: 16px 0px 16px 0px;
+    }
+  }
 }
 </style>
