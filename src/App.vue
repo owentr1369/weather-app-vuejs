7474a4f37ce734c58307cebdf7fc31718d53d004
owentr1369 <tamvaa1306@gmail.com>
2022-05-28 10:29:23 +0700

    styling weather box

--- src/App.vue
+++ src/App.vue
@@ -82,4 +82,44 @@ main {
     }
   }
 }
+.weather-wrap {
+  .location-box {
+    .location {
+      color: #fff;
+      font-size: 32px;
+      font-weight: 500;
+      text-align: center;
+      text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    }
+    .date {
+      color: #fff;
+      font-size: 20px;
+      font-weight: 300;
+      text-align: center;
+      font-style: italic;
+    }
+  }
+  .weather-box {
+    text-align: center;
+    .temp {
+      display: inline-block;
+      padding: 10px 25px;
+      color: #fff;
+      font-size: 102px;
+      font-weight: 900;
+      text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+      background-color: rgba(255, 255, 255, 0.25);
+      margin: 30px 0px;
+      border-radius: 16px;
+      box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    .weather {
+      color: #fff;
+      font-size: 48px;
+      font-weight: 700;
+      font-style: italic;
+      text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    }
+  }
+}
 </style>
